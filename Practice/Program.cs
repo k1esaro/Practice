39a283fe72d530ffdac5925b1ba244e6393d1f75
Kiesaro <freestylerspb@gmail.com>
2023-04-07 21:39:07 +0300

    second commit

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,2 +1,24 @@
 ﻿Console.Clear();
-string [] array;
+string [] array = {"hello", "2", "world", ":-"};
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 3)
+    {
+        count++;
+    } 
+}
+string [] result = new string[count];
+int k = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 3)
+    {
+        result[k]=array[i];
+        k++;
+    } 
+}
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write($"{result[i]} ");
+}
